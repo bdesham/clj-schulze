e0d2a55b418bbfccbbca9c63ad9c550e4d282f62
Benjamin Esham <bdesham@gmail.com>
2011-05-29 22:41:37 -0500

    Functions to add missing candidates and produce canonical ballots

--- src/clj_condorcet/core.clj
+++ src/clj_condorcet/core.clj
@@ -1,22 +1,41 @@
-(ns clj-condorcet.core)
+(ns clj-condorcet.core
+  (:use [clojure.set :only (difference)]))
 
 (defn flatten-sets [v]
-  "Like flatten, but also pulls elements out of sets. (Sets which are not
-  top-level in the input vector are left untouched.)"
-  (flatten (map #(if (set? %)
-                   (seq %)
-                   %)
-                v)))
+  "Like flatten, but pulls elements out of sets instead of sequences."
+  (filter (complement set?)
+          (rest (tree-seq set? seq (set v)))))
 
 (defn validate-ballot [ballot candidates]
   (and (vector? ballot)
        (when-let [fb (flatten-sets ballot)]
-         (and (distinct? fb)
+         (and (apply distinct? fb)
               (every? #(candidates %) fb)))))
+
+(defn validate-ballots [ballots candidates]
+  (every? #(validate-ballot % candidates)))
 
 (defn validate-candidates [candidates]
   (and (set? candidates)
        (every? keyword? candidates)))
+
+(defn add-missing-candidates [ballot candidates]
+  (let [extras (difference candidates (flatten-sets ballot))]
+    (if (not (empty? extras))
+      (conj ballot extras)
+      ballot)))
+
+(defn canonical-ballot [ballot]
+  "Return the ballot with the following changes: all keywords not in a set are
+  converted to one-element sets; any nested sets (?!) are flattened; empty sets
+  are removed."
+  (map #(if (keyword? %)
+          (set (vector %))
+          (set (flatten-sets %)))
+       (filter #(or (keyword %) (seq %)) ballot)))
+
+(defn canonical-ballots [ballots]
+  (map canonical-ballot ballots))
 
 ; vim: tw=80
 ; intended to be viewed with a window width of 108 columns
