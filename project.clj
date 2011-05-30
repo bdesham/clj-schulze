69b25ef7e55f3861e11e9722d7d009569a1701c0
Benjamin Esham <bdesham@gmail.com>
2011-05-29 22:49:47 -0500

    Use marginalia and add doc strings

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject clj-condorcet "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+  :description "A Clojure implementation of the Condorcet voting method"
   :dependencies [[org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+            :dev-dependencies [[marginalia "0.2.3"]]
+            :main clj-condorcet.core)
--- src/clj_condorcet/core.clj
+++ src/clj_condorcet/core.clj
@@ -1,25 +1,37 @@
 (ns clj-condorcet.core
   (:use [clojure.set :only (difference)]))
 
+;; # Utility functions
+
 (defn flatten-sets [v]
   "Like flatten, but pulls elements out of sets instead of sequences."
   (filter (complement set?)
           (rest (tree-seq set? seq (set v)))))
 
+;; # Validation, canonicalization, etc.
+
 (defn validate-ballot [ballot candidates]
+  "Make sure that the ballot is a vector; contains no duplicate entries,
+  including in nested sets; and contains only entries which appear in the set of
+  candidates."
   (and (vector? ballot)
        (when-let [fb (flatten-sets ballot)]
          (and (apply distinct? fb)
               (every? #(candidates %) fb)))))
 
 (defn validate-ballots [ballots candidates]
+  "Check every ballot in a sequence with validate-ballot."
   (every? #(validate-ballot % candidates)))
 
 (defn validate-candidates [candidates]
+  "Make sure that the candidates set *is* a set and consists entirely of
+  keywords."
   (and (set? candidates)
        (every? keyword? candidates)))
 
 (defn add-missing-candidates [ballot candidates]
+  "If any valid candidates don't appear on the ballot, add them in a set at the
+  end of the ballot."
   (let [extras (difference candidates (flatten-sets ballot))]
     (if (not (empty? extras))
       (conj ballot extras)
@@ -35,6 +47,7 @@
        (filter #(or (keyword %) (seq %)) ballot)))
 
 (defn canonical-ballots [ballots]
+  "Call canonical-ballot for every ballot in a sequence."
   (map canonical-ballot ballots))
 
 ; vim: tw=80
