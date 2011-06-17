415244bad8165816bdc487cee153293b3e404d36
Benjamin Esham <bdesham@gmail.com>
2011-06-17 13:54:52 -0500

    Use the latest release of Marginalia

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A Clojure implementation of the Schulze voting method"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
-            :dev-dependencies [[marginalia "0.2.3"]]
+            :dev-dependencies [[marginalia "0.5.1"]]
             :main clj-schulze.core)
